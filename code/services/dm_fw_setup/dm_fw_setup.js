45b002387fb7a68488adf3333cbd58979b566a22
Jim Bouquet <jbouquet@clearblade.com>
2025-02-28 15:49:23 -0600

    Initial component code

--- code/services/dm_fw_setup/dm_fw_setup.js
+++ code/services/dm_fw_setup/dm_fw_setup.js
@@ -6,6 +6,44 @@
 
 function dm_fw_setup(req, resp) {
   const params = req.params;
-  //component setup behavior here initialize any external databases, bucket sets, etc.
-  resp.success('Success');
+
+  function applyPermissionsToRole(roleId) {
+    //TODO - Add collection permissions
+    //Add topic (publish) permissions
+      return ClearBladeAsync.Role(roleId).setPermissions([
+        {
+          "type": "dashboard",
+          "name": "software_updater",
+          "level": ClearBladeAsync.Permissions.READ
+        },
+        {
+          "type": "dashboard",
+          "name": "software_uploader",
+          "level": ClearBladeAsync.Permissions.READ
+        }
+      ])
+  }
+
+  //Add execute permissions to editor and administrator roles for checkEdgeDeviceStatus and createOpcuaMap
+  //Add portal permissions to editor and administrator roles
+  ClearBladeAsync.Roles().read(ClearBladeAsync.Query().equalTo("name", "Administrator").or(ClearBladeAsync.Query().equalTo("name", "Editor")))
+  .then(function(data) {
+    return Promise.all(data
+      .map(function (role) {
+        console.debug("Applying permissions for role " + role.name);
+        return applyPermissionsToRole(role.role_id);
+      }));
+  })
+  .then(function (results) {
+
+    //TODO - Add portals to external links in system_info
+
+
+    console.debug(results);
+    resp.success('Success');
+  })
+  .catch(function (error) {
+    console.error("Error applying permissions to roles: " + JSON.stringify(error));
+    resp.error("Error applying permissions to roles: " + JSON.stringify(error));
+  });
 }
--- code/services/dm_fw_teardown/dm_fw_teardown.js
+++ code/services/dm_fw_teardown/dm_fw_teardown.js
@@ -6,6 +6,38 @@
 
 function dm_fw_teardown(req, resp) {
   const params = req.params;
-  //component teardown behavior here. Undo any setup done in the setup service
-  resp.success('Success');
+
+  function removePermissionsFromRole(roleId) {
+    return ClearBladeAsync.Role(roleId).setPermissions([
+      {
+        "type": "dashboard",
+        "name": "software_updater",
+        "level": 0
+      },
+      {
+        "type": "dashboard",
+        "name": "software_uploader",
+        "level": 0
+      }
+    ])
+}
+
+  //Remove execute permissions from editor and administrator roles for checkEdgeDeviceStatus and createOpcuaMap
+  //Remove portal permissions from editor and administrator roles
+  ClearBladeAsync.Roles().read(ClearBladeAsync.Query().equalTo("name", "Administrator").or(ClearBladeAsync.Query().equalTo("name", "Editor")))
+  .then(function(data) {
+    return Promise.all(data
+      .map(function (role) {
+        console.debug("Applying permissions for role " + role.name);
+        return removePermissionsFromRole(role.role_id);
+      }));
+  })
+  .then(function (results) {
+    console.debug(results);
+    resp.success('Success');
+  })
+  .catch(function (error) {
+    console.error("Error applying permissions to roles: " + JSON.stringify(error));
+    resp.error("Error applying permissions to roles: " + JSON.stringify(error));
+  });
 }
